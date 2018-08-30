816edb30a7c0cb13e8af885ed05998fc037cfafa
louisburette <louis.burette@ieseg.fr>
2018-08-30 19:09:15 +0200

    profile ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :views
   devise_for :users
   root to: 'pages#home'
   resources :trips
   get '/dashboard', to: 'accounts#dashboard'
-  get '/profile', to: 'profile#edit'
+  resources :profiles, only: [:edit]
+  get '/profile', to: 'devise/registrations#edit', as: 'profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
