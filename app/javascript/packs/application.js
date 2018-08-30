6e869cdaee6f82da01b67167a024e2e71b115f43
claire thomas <claire_thomas@hotmail.fr>
2018-08-30 19:38:25 +0200

    ok front form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,12 @@
 import "bootstrap";
 import { autocomplete } from '../components/autocomplete';
 autocomplete();
+
+const clBtn = document.getElementById('user_photo')
+if (clBtn) {
+  const loader = document.getElementById('photo-loader')
+  loader.addEventListener('click', (e) => {
+    e.preventDefault()
+    clBtn.click()
+  })
+}
