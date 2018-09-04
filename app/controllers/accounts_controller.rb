7dca3be43ff87d3f86759f4e5d815ccd9c620c67
claire thomas <claire_thomas@hotmail.fr>
2018-09-04 12:48:00 +0200

    dashboard

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,10 @@
 class AccountsController < ApplicationController
   skip_after_action :verify_authorized
+
   def dashboard
     array_trips_id = []
     current_user.profile.travellers.each do |traveller|
-     array_trips_id << traveller.trip_id
+    array_trips_id << traveller.trip_id
     end
     @trips = []
     array_trips_id.each do |id|
