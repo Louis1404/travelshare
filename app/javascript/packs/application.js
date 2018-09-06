8eb7794bfc0aac187e384397916568ec3fcb4184
louisburette <louis.burette@ieseg.fr>
2018-09-06 14:34:53 +0200

    map with JS style started

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,16 +17,30 @@ if (clBtn) {
 
   // TRIP FINDER
   // fonction de comparaison
-  function compare(a, b) {
-  if (a.price < b.price)
-    return -1
-  if (a.price > b.price)
-    return 1
-  return 0
-  }
+function compare(a, b) {
+if (a.price < b.price)
+  return -1
+if (a.price > b.price)
+  return 1
+return 0
+}
+
+function locateCityOnMap(city) {
+  map = window.map
+  const geocoder = new google.maps.Geocoder()
+  geocoder.geocode( { 'address': city}, function(results) {
+    const coords = {
+      lat: results[0].geometry.location.lat(),
+      lng: results[0].geometry.location.lng()
+    }
+    map.addMarkers([coords])
+    // map.setCenter(coords)
+  })
+}
+
 
   // compare les resultats de l'api
-  function resultComparator(object) {
+function resultComparator(object) {
   // console.log("OBJECT", object)
   const prices = {}
   for (var key in object) {
@@ -56,10 +70,10 @@ if (clBtn) {
   cityCard.style.backgroundImage = bgStyle
   cityCard.querySelector('h3').innerText = destCity.toUpperCase()
   return result
-  }
+}
 
   // recupere les villes des participants et fait la recherche des prix
-  async function getInfos(profiles) {
+async function getInfos(profiles) {
   const results = {}
   for (const profile of profiles) {
     const { data } = await axios.get(`/getinfos?city=${profile.city}`);
@@ -67,81 +81,97 @@ if (clBtn) {
     results[`${profile.city}`] = data["trip"][`${profile.city}`]
   }
   return results
-  }
+}
 
-  function addTripToDom(profile, data) {
+function addTripToDom(profile, data) {
   // for (const profile of profiles) {
     // console.log(`${profile.to_json}`)
     // const traveller = `${profile.to_json}`
     // const way = traveller.way
-    const cardHTML = createCard(profile, data)
-    const tripDetails = document.querySelector('.trip-details')
-    tripDetails.insertAdjacentHTML("beforeend", cardHTML)
-  }
+  const cardHTML = createCard(profile, data)
+  const tripDetails = document.querySelector('.trip-details')
+  tripDetails.insertAdjacentHTML("beforeend", cardHTML)
+}
 
-  async function getTrips(trip, profiles, destCity) {
-    for (const profile of profiles) {
+async function getTrips(trip, profiles, destCity) {
+  for (const profile of profiles) {
     const { data } = await axios.get(`/getinfos?city=${profile.city}&destination=${destCity}&id=${profile.id}&trip=${trip.id}`);
     const data_exploitable = data.trip[`${profile.city}`][destCity]
+    if (window.map.markers.length === 1) {
+      const coords = window.map.markers[0].position
+      window.destCityCoords = coords
+    }
     addTripToDom(profile, data_exploitable)
     const map = window.map
+    // const contentString = "<h1>rien ?</h1>"
+    // const infoWindow = new google.maps.InfoWindow({
+    //     content: contentString
+    // })
     const coords = {
       lat: profile.latitude,
       lng: profile.longitude
+      // infoWindow: '<h1>RIEN</h1>'
     }
-    map.setCenter(coords)
+    // map.setCenter(coords)
     map.addMarkers([coords])
-    map.setZoom(10)
-  }
-  }
-
-
-  function fontAwesome(data) {
-    if (data['transport'].includes("Fly")) {
-      return `<i class="fas fa-plane"></i>`
-    } else if (data[`transport`].includes("Train")) {
-      return `<i class="fas fa-subway"></i>`
-    } else {
-      return data['transport']
+    const lineOptions = {
+      path: [
+        [window.destCityCoords.lat(), window.destCityCoords.lng()],
+        [coords.lat, coords.lng]
+      ],
+      strokeColor: 'red'
     }
-  };
-
-  function usdToEuros(data) {
-    return `${Math.round(data['price'] * 0.86018)}`
+    map.drawPolyline(lineOptions)
+    // map.setZoom(10)
   }
+}
 
-  function mintoRealTime(data) {
-    var hours = Math.floor(data['time'] / 60);
-    var minutes = (data['time'] % 60);
 
-    return `${hours} h ${minutes}`
+function fontAwesome(data) {
+  if (data['transport'].includes("Fly")) {
+    return `<i class="fas fa-plane"></i>`
+  } else if (data[`transport`].includes("Train")) {
+    return `<i class="fas fa-subway"></i>`
+  } else {
+    return data['transport']
   }
+}
+
+function usdToEuros(data) {
+  return `${Math.round(data['price'] * 0.86018)}`
+}
+
+function mintoRealTime(data) {
+  var hours = Math.floor(data['time'] / 60);
+  var minutes = (data['time'] % 60);
+
+  return `${hours} h ${minutes}`
+}
 
   // lance le tout
-  async function bestMatchFinder() {
-  const results = await getInfos(gon.profiles)
-  // console.log("API CALL RESPONSE", results)
-  const bestMatch = resultComparator(results)
-  // console.log("BEST MATCH", bestMatch)
-  const trips = getTrips(gon.trip, gon.profiles, bestMatch.city)
-  }
+async function bestMatchFinder() {
+const results = await getInfos(gon.profiles)
+// console.log("API CALL RESPONSE", results)
+const bestMatch = resultComparator(results)
+locateCityOnMap(bestMatch.city)
+const trips = getTrips(gon.trip, gon.profiles, bestMatch.city)
+}
 
-  const createCard = (profile, data) => {
-    return `
-      <div class="trip-detail-content">
-        <div class="traveller-name">
-          <h5><strong>${profile.first_name} ${profile.last_name}</strong></h5>
-        </div>
-        <div class="traveller-info">
-        <div class="travel-type">
-          <p>${fontAwesome(data)}</p>
-        </div>
-        <div class="travel-time-price"></div>
-          <p>${mintoRealTime(data)} / <strong>${usdToEuros(data)} €</strong></p>
-        </div>
+const createCard = (profile, data) => {
+  return `
+    <div class="trip-detail-content">
+      <div class="traveller-name">
+        <h5><strong>${profile.first_name} ${profile.last_name}</strong></h5>
       </div>
-    `
-  }
+      <div class="traveller-info">
+      <div class="travel-type">
+        <p>${fontAwesome(data)}</p>
+      </div>
+      <div class="travel-time-price"></div>
+        <p>${mintoRealTime(data)} / <strong>${usdToEuros(data)} €</strong></p>
+      </div>
+    </div>
+  `
+}
 
 bestMatchFinder()
-// window.map.
