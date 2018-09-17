8f3303794aae1b73960674f2cbe247912399be4b
Adrien Lefrancq <irdane2111@gmail.com>
2018-09-17 16:34:23 +0200

    show from dashboard good, new trip cassé

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -20,19 +20,14 @@ class TripsController < ApplicationController
       @trip.travellers.each do |t|
         @travellers << t
       end
+      @markers = []
+      @markers = @travellers.map do |t|
+        {
+          lat: t.profile.latitude,
+          lng: t.profile.longitude
+        }
+      end
     end
-
-    # @travellers.each do |traveller|
-    #   @profiles << Profile.find(traveller.profile_id)
-    # end
-    # @markers = @profiles.map do |profile|
-    #   @geocod = Geocoder.search(profile.city)
-    #   {
-    #     lat: @geocod[0].data["geometry"]["location"]["lat"],
-    #     lng: @geocod[0].data["geometry"]["location"]["lng"]#,
-    #     # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-    #   }
-    # end
 
     # @information_destination = BestMatchCaller.new(@trip, @travellers).call
     # @ways = create_ways(@information_destination)
